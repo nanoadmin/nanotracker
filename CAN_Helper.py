57239ad2e7428c0c6cf04c66a077a4f04d38f5d9
nanoadmin <admin@nanosoft.com.au>
2017-08-14 11:14:34 +0800

    showing graydon / adding file for first time

---
+++ CAN_Helper.py
@@ -0,0 +1,19 @@
+
+########################################################################
+class MessageConverter:
+    """Collection of static methods etc which can be used to convert messages to CAN like hex values"""
+
+    #----------------------------------------------------------------------
+    def __init__(self):
+        """Constructor"""
+        
+    def DateConvert(thisDate):
+        
+        return "not implemented"
+    
+        
+        
+    
+    
+
+
