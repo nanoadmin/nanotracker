be5b38003ba9027288ccd3b340024d345e6166a6
nanoadmin <admin@nanosoft.com.au>
2017-09-18 07:50:10 +0800

    fixed up iossues with gp test

--- sensors/gps/gps_test.py
+++ sensors/gps/gps_test.py
@@ -6,7 +6,7 @@ from microtack_gps import L80GPS
 print("example= python3 gps_test.py ttyAMA0")
 
 
-arg =  print(sys.argv[1])
+arg =  sys.argv[1]
 print ("the argument given = {0}".format( arg))
 
 i = 1
@@ -17,19 +17,21 @@ print ("will try 10 attempts at GPS")
 
 devicename = "/dev/{0}".format(arg)
 
+print (devicename)
+
 while i<=10:
     
     try:
         
         print ("attempt {0}".format( i))
-        gps = L80GPS()
+        gps = L80GPS(devicename)
         x = gps.get_gpgll()
         lat = x['latitude']
         print(x)               
         
         
-    except:
-        print ("exception caused")
+    except Exception as e:
+        print (e)
     
     i = i + 1
 
