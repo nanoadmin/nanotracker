399f40f5f44a95bdcf33869284bc4b4a687dd94b
nanoadmin <admin@nanosoft.com.au>
2017-09-28 08:04:25 +0800

    rc_local file added

---
+++ rc_local_run.py
@@ -0,0 +1,14 @@
+import os
+import time 
+
+
+print('waiting 10 seconds in hope that device will connect to internet ')
+
+time.sleep(10)
+
+print('get latest version of source for devices_will_pull_this branch')
+
+os.system('git pull origin devices_will_pull_this')
+
+
+
