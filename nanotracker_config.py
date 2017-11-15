1cc7ace1b50ad8512d89b41a0ecfd0486971a378
nanoadmin <30917912+nanoadmin@users.noreply.github.com>
2017-11-16 06:43:27 +0800

    Update nanotracker_config.py
    added CAN standards as options to the config file.

--- nanotracker_config.py
+++ nanotracker_config.py
@@ -29,6 +29,9 @@ API_POST_TIMER = 5  # number of seconds between each post request to the api
 #                        #
 ##########################
 
+CAN0_STANDARD = 'Zagro125'
+CAN1_STANDARD = 'Zagro500'
+
 IS_DUAL_CAN = True # if this is the sinlge CAN device then we will mark this as false
 #below, even if the canbus is not going to be used, please give it a summy name and bitrate
 CAN0_NAME = 'can0'
