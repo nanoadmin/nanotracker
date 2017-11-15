55ea66a81aeea93f194dd68b2c3641883bbe21da
nanoadmin <30917912+nanoadmin@users.noreply.github.com>
2017-11-16 07:18:34 +0800

    Update CAN_messagesend.py

--- CAN_messagesend.py
+++ CAN_messagesend.py
@@ -1,4 +1,3 @@
-#test
 #!/usr/bin/python3
 
 import sys
@@ -26,31 +25,37 @@ except OSError:
     exit()
 
 
-# Main loop
-try:
+while True:   
+
+    # Main loop
+    try:
+        
+        bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
+
+        SEND_REQUESTS = {
+            0x1CEA0000: [[0xA4,0xFE,0x00],[0xDF,0xFE,0x00],[0xE5,0xFE,0x00],[0xE7,0xFE,0x00],[0xEE,0xFE,0x00]]
+        }
+
+        while True:
+
+            for element in itertools.cycle(SEND_REQUESTS[PID_REQUEST]):
+
+                msg = can.Message(arbitration_id=PID_REQUEST,data=element,extended_id=True)
+                bus.send(msg)
+
+                time.sleep(0.3)
 
 
-    SEND_REQUESTS = {
-
-    0x1CEA0000: [[0xA4,0xFE,0x00],[0xDF,0xFE,0x00],[0xE5,0xFE,0x00],[0xE7,0xFE,0x00],[0xEE,0xFE,0x00]]
-
-
-    }
-
-
-    while True:
-
-
-        for element in itertools.cycle(SEND_REQUESTS[PID_REQUEST]):
-
-            msg = can.Message(arbitration_id=PID_REQUEST,data=element,extended_id=True)
-            bus.send(msg)
-
-            time.sleep(0.3)
+    except KeyboardInterrupt:
+        #Catch keyboard interrupt
+        #os.system("sudo /sbin/ip link set can0 down")
+        print('\n\rKeyboard interrtupt')
+        return        
+        
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
 
 
-except KeyboardInterrupt:
-    #Catch keyboard interrupt
-    #os.system("sudo /sbin/ip link set can0 down")
-    print('\n\rKeyboard interrtupt')
+        
+        
 
