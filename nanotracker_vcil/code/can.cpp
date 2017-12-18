73b9404331263f8412c6985f73ca8bb068df7756
nanoadmin <30917912+nanoadmin@users.noreply.github.com>
2017-12-18 13:35:58 +0800

    Update can.cpp

--- nanotracker_vcil/code/can.cpp
+++ nanotracker_vcil/code/can.cpp
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <net/if.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
 #include "common.h"
 #include "pthread.h"
 #include "SUSI_IMC.h"
