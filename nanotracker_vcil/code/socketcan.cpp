58d76874da952c36acb99a083f5f9c24dc465a6f
nanoadmin <30917912+nanoadmin@users.noreply.github.com>
2017-12-21 12:12:18 +0800

    Update socketcan.cpp

--- nanotracker_vcil/code/socketcan.cpp
+++ nanotracker_vcil/code/socketcan.cpp
@@ -52,7 +52,7 @@ static void *can_read_thread(void *p)
 		IMC_CAN_MSG_OBJECT message;
 		while( (result = SUSI_IMC_CAN_Read(&message)) == IMC_ERR_NO_ERROR)
 		{
-			char can_cmd[200] = "":
+			char can_cmd[200] = "";
 			
 			sprintf(can_cmd, "candump vcan%i %lx#",  (message.can_bus_number - 1), message.id);
 			
